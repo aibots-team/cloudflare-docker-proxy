698f97cf32d64c79c974a4fed891e9869a773431
aibots-team <111552083+aibots-team@users.noreply.github.com>
2024-06-18 12:59:00 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.e-whisper.com": "https://registry-1.docker.io",
-  "quay.e-whisper.com": "https://quay.io",
-  "gcr.e-whisper.com": "https://gcr.io",
-  "k8s-gcr.e-whisper.com": "https://k8s.gcr.io",
-  "k8s.e-whisper.com": "https://registry.k8s.io",
-  "ghcr.e-whisper.com": "https://ghcr.io",
-  "cloudsmith.e-whisper.com": "https://docker.cloudsmith.io",
+  "docker.neomate.cc": "https://registry-1.docker.io",
+  "quay.neomate.cc": "https://quay.io",
+  "gcr.neomate.cc": "https://gcr.io",
+  "k8s-gcr.neomate.cc": "https://k8s.gcr.io",
+  "k8s.neomate.cc": "https://registry.k8s.io",
+  "ghcr.neomate.cc": "https://ghcr.io",
+  "cloudsmith.neomate.cc": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
